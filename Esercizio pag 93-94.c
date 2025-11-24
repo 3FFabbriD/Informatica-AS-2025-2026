eb4cd1026bf1394350832b4d8d451882cdf6b56d
[200~3FFabbriD~ <daniele.fabbri.superdany@gmail.com>
2025-11-24 17:47:15 +0100

    se non faccio questo commit non funziona niente

--- Esercizio pag 93-94.c
+++ Esercizio pag 93-94.c
@@ -4,11 +4,28 @@
 void massimo(){
     int num1,num2,num3,max; 
     printf("inserire il primo numero");
-    scanf("%d", num1); 
+    scanf("%d", &num1); 
 
     printf("inserire il secondo numero");
-    scanf("%d", num2);
+    scanf("%d", &num2);
     
     printf("inserire il terzo numero");
-    scanf("%d", num3); 
+    scanf("%d", &num3); 
+
+    max=num1;
+
+    if(num2>max){
+        max=num2;
+    }
+    if(num3>max){
+        max=num3;
+    }
+    printf("il numero maggiore è %d\n", max); 
+
+}
+
+int main (){
+    massimo();
+    system("pause"); 
+    return 0; 
 }
